fb664211a667024edd420629fa4d354e3ab2b683
IsaacDrp <isaacdp09@gmail.com>
2024-11-02 19:56:38 -0600

    modificacion de nav

--- public/js/principal/togglePanel.js
+++ public/js/principal/togglePanel.js
@@ -1,7 +1,19 @@
-function togglePanel() {
-    const panel = document.getElementById("side-panel");
-    if (panel) {
-      panel.classList.toggle("open");
-    }
-  }
+document.addEventListener('DOMContentLoaded', function () {
+    // Selecciona todas las opciones de filtro
+    const filterOptions = document.querySelectorAll('.filter-option');
+  
+    filterOptions.forEach(option => {
+      option.addEventListener('click', () => {
+        // Cierra cualquier opción abierta
+        filterOptions.forEach(otherOption => {
+          if (otherOption !== option && otherOption.classList.contains('open')) {
+            otherOption.classList.remove('open');
+          }
+        });
+  
+        // Alterna la clase 'open' en la opción seleccionada
+        option.classList.toggle('open');
+      });
+    });
+  });
   
